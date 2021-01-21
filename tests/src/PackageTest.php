8072e1d80efabec842480b1fc91b12db4b07140f
Bernhard Efler <1978420+bernhard-efler@users.noreply.github.com>
2021-01-21 09:00:59 +0100

    update for servicemanger 0.2

--- tests/src/PackageTest.php
+++ tests/src/PackageTest.php
@@ -9,9 +9,6 @@ declare(strict_types=1);
 
 namespace Ixocreate\Test\Validaton;
 
-use Ixocreate\Application\Configurator\ConfiguratorRegistryInterface;
-use Ixocreate\Application\Service\ServiceRegistryInterface;
-use Ixocreate\ServiceManager\ServiceManagerInterface;
 use Ixocreate\Validation\Package;
 use PHPUnit\Framework\TestCase;
 
@@ -22,19 +19,10 @@ class PackageTest extends TestCase
      */
     public function testPackage()
     {
-        $configuratorRegistry = $this->getMockBuilder(ConfiguratorRegistryInterface::class)->getMock();
-        $serviceRegistry = $this->getMockBuilder(ServiceRegistryInterface::class)->getMock();
-        $serviceManager = $this->getMockBuilder(ServiceManagerInterface::class)->getMock();
-
         $package = new Package();
-        $package->configure($configuratorRegistry);
-        $package->addServices($serviceRegistry);
-        $package->boot($serviceManager);
 
-        $this->assertNull($package->getBootstrapItems());
-        $this->assertNull($package->getConfigDirectory());
-        $this->assertNull($package->getConfigProvider());
-        $this->assertNull($package->getDependencies());
+        $this->assertEmpty($package->getBootstrapItems());
+        $this->assertEmpty($package->getDependencies());
         $this->assertDirectoryExists($package->getBootstrapDirectory());
     }
 }
